97416d808790ff44692b1c864aff759290488959
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 18:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 # x = st.slider("Select a value")
 # st.write(x, "squared is", x * x)
 
-st.title('This is a title')
-st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
+st.title('Video Object Detection')
+
