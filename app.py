b36769730d197a51c392a4d2c00433637ba22c9a
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 19:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,27 @@ st.title('Video Object Detection')
 title = st.text_input('Enter things that need to be detected', '')
 st.write(title)
 
-st.title("Upload and Play Video")
-uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi"])
+import streamlit as st
+import pandas as pd
+from io import StringIO
 
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    st.video(uploaded_file)
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 
 
