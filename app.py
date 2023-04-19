262fab7ceaa16ee37bb4af1e0a236bd92f248bce
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 19:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import cv2
 # x = st.slider("Select a value")
 # st.write(x, "squared is", x * x)
 
@@ -7,18 +8,11 @@ st.title('Video Object Detection')
 title = st.text_input('Enter things that need to be detected', '')
 st.write(title)
 
+st.title("Upload and Play Video")
+uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi"])
 
-import cv2
+if uploaded_file is not None:
+    st.video(uploaded_file)
 
-def main():
-    st.title("Upload and Play Video")
-
-    uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi"])
-
-    if uploaded_file is not None:
-        st.video(uploaded_file)
-
-if __name__ == '__main__':
-    main()
 
 
