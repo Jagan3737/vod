995a1404ca6644c9848bb7a12a05ab551e7383b7
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 19:09:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ st.title('Video Object Detection')
 title = st.text_input('Enter things that need to be detected', '')
 st.write(title)
 
+
+with st.form("upload-form", clear_on_submit=True):
+                uploaded_file = st.file_uploader(model.upload_button_text_desc, accept_multiple_files=False,
+                                                 type=['png', 'jpg', 'jpeg'],
+                                                 help=model.upload_help)
+                submitted = st.form_submit_button(model.upload_button_text)
+
+                if submitted and uploaded_file is not None:
+                    ret = self.upload_file(uploaded_file)
+
