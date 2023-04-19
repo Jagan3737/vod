cfb64dfaa69adb3954f3beab501d05d587f6f2bc
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 19:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,11 @@ if uploaded_file is not None:
     
 
 
-image = Image.open(dataframe)
-
-st.image(image, caption='Sunrise by the mountains')
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
 
 
     
