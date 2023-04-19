51680715d7e8f41430a7664d340637b730be6923
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 18:49:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+# x = st.slider("Select a value")
+# st.write(x, "squared is", x * x)
+
+st.title('This is a title')
+st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
