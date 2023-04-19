30e41c7fc010554a2b3c148c9a1518f2a0f6c5d6
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 21:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,36 +11,40 @@ title = st.text_input('Enter things that need to be detected', '')
 st.write(title)
 
 
+f = st.file_uploader("Upload file")
+tfile = tempfile.NamedTemporaryFile(delete=False)
+tfile.write(f.read())
+vf = cv.VideoCapture(tfile.name)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
+#     # To read file as bytes:
+#     bytes_data = uploaded_file.getvalue()
+#     st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+#     # To convert to a string based IO:
+#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+#     st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+#     # To read file as string:
+#     string_data = stringio.read()
+#     st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+#     # Can be used wherever a "file-like" object is accepted:
+#     dataframe = pd.read_csv(uploaded_file)
+#     st.write(dataframe)
     
 
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     st.write("filename:", uploaded_file.name)
+#     st.write(bytes_data)
     
-    image = Image.open('sunrise.jpg')
+#     image = Image.open('sunrise.jpg')
 
-    st.image(image, caption='Sunrise by the mountains')
+#     st.image(image, caption='Sunrise by the mountains')
              
              
 
