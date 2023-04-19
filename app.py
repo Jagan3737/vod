1b933ecd83204d7cf2c869448e8d472ae7a9eb82
Jagannathan K <jagan.nathan.jn24@gmail.com>
2023-04-19 19:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,19 +43,7 @@ for uploaded_file in uploaded_files:
     st.image(image, caption='Sunrise by the mountains'
              
              
-import cv2
-import numpy as np
-import streamlit as st
 
-uploaded_file = st.file_uploader("Choose a image file", type="jpg")
-
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="BGR")
 
 
     
